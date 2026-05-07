5307481a1ce198b7c0dfb9db8ffb1c66b468d617
Miguel <miguelaugustofc@gmail.com>
2026-05-07 12:35:05 -0300

    adiciona a classe categoria

---
+++ src/br/edu/biblioteca/model/Categoria.java
@@ -0,0 +1,15 @@
+package br.edu.biblioteca.model;
+
+public class Categoria {
+
+    private int id;
+    private String nome;
+
+    public Categoria() {
+    }
+
+    public Categoria(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+}
