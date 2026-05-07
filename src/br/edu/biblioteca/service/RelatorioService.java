95e94075def6439498f84248b595e642124f392d
Miguel <miguelaugustofc@gmail.com>
2026-05-07 12:49:33 -0300

    Create RelatorioService.java

---
+++ src/br/edu/biblioteca/service/RelatorioService.java
@@ -0,0 +1,20 @@
+package br.edu.biblioteca.service;
+
+public class RelatorioService {
+
+    public void topMaisEmprestados() {
+        System.out.println("Relatório de livros mais emprestados");
+    }
+
+    public void emAtraso() {
+        System.out.println("Relatório de atrasos");
+    }
+
+    public void usuariosComMaisAtrasos() {
+        System.out.println("Usuários com mais atrasos");
+    }
+
+    public void estatisticasMensais() {
+        System.out.println("Estatísticas mensais");
+    }
+}
