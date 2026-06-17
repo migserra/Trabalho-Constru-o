56a76b0f192643274c1e838eb43081dd1aaa0c31
Miguel <miguelaugustofc@gmail.com>
2026-06-17 13:42:27 -0300

    Create ReservaRepository.java

---
+++ src/br/edu/biblioteca/repository/ReservaRepository.java
@@ -0,0 +1,12 @@
+package br.edu.biblioteca.repository;
+
+public class ReservaRepository {
+
+    public void salvar() {
+        System.out.println("Reserva salva");
+    }
+
+    public void carregar() {
+        System.out.println("Reservas carregadas");
+    }
+}
