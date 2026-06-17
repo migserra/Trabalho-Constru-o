499997bd8196481923c56071f2746af0566dab11
Miguel <miguelaugustofc@gmail.com>
2026-06-17 13:42:43 -0300

    Create ExemplarRepository.java

---
+++ src/br/edu/biblioteca/repository/ExemplarRepository.java
@@ -0,0 +1,12 @@
+package br.edu.biblioteca.repository;
+
+public class ExemplarRepository {
+
+    public void salvar() {
+        System.out.println("Exemplar salvo");
+    }
+
+    public void carregar() {
+        System.out.println("Exemplares carregados");
+    }
+}
