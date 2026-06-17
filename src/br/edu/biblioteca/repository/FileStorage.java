23565e1140643e99990d8919ea774b574c71de38
Miguel <miguelaugustofc@gmail.com>
2026-06-17 13:42:56 -0300

    Add FileStorage class for file operations

---
+++ src/br/edu/biblioteca/repository/FileStorage.java
@@ -0,0 +1,12 @@
+package br.edu.biblioteca.repository;
+
+public class FileStorage {
+
+    public void salvarArquivo() {
+        System.out.println("Arquivo salvo");
+    }
+
+    public void carregarArquivo() {
+        System.out.println("Arquivo carregado");
+    }
+}
