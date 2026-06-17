d5a5ecdd74c0d53d91250f5e97c32631699479b8
Miguel <miguelaugustofc@gmail.com>
2026-06-17 13:42:09 -0300

    Create UsuarioRepository.java

---
+++ src/br/edu/biblioteca/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package br.edu.biblioteca.repository;
+
+public class UsuarioRepository {
+
+    public void salvar() {
+        System.out.println("Usuário salvo");
+    }
+
+    public void carregar() {
+        System.out.println("Usuários carregados");
+    }
+}
