4174869c3829624f97acbbccf89ddeddfd4d4fe1
Miguel <miguelaugustofc@gmail.com>
2026-06-17 13:41:46 -0300

    Create LivroRepository.java

---
+++ src/br/edu/biblioteca/repository/LivroRepository.java
@@ -0,0 +1,12 @@
+package br.edu.biblioteca.repository;
+
+public class LivroRepository {
+
+    public void salvar() {
+        System.out.println("Livro salvo");
+    }
+
+    public void carregar() {
+        System.out.println("Livros carregados");
+    }
+}
